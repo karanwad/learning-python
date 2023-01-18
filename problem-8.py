d53880d72168b5fead901149db9ce20c02fc4df2
karanwad <johnnyreb242@gmail.com>
2023-01-18 20:19:17 +0000

    push code up

---
+++ problem-8.py
@@ -0,0 +1,12 @@
+# print all the items in this list 
+
+list = [5, 10, 20, 4]
+
+for x in list:
+    print(x)
+
+ #fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     if x == "banana":
+#         continue
+#     print(x)
---
+++ problem-9.py
@@ -0,0 +1,7 @@
+#write a function that will take a list as an argument and print out each item in it
+
+list = [5, 10, 20, 4]
+def printitem(list):
+    for x in list:
+        print(X)
+
